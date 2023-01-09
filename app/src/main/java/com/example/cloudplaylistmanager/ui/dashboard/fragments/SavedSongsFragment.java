9ffd21ecf1f8bca98b5fbe970a9e07348399ce0d
ErikNguyen20 <20nguyened@gmail.com>
2023-01-09 00:13:56 -0800

    Created popup menu for creating new songs
    Created the popup UI for entering the URL of a song or importing as a file.

--- app/src/main/java/com/example/cloudplaylistmanager/ui/dashboard/fragments/SavedSongsFragment.java
+++ app/src/main/java/com/example/cloudplaylistmanager/ui/dashboard/fragments/SavedSongsFragment.java
@@ -1,5 +1,6 @@
 package com.example.cloudplaylistmanager.ui.dashboard.fragments;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -12,6 +13,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.cloudplaylistmanager.MusicPlayer.MediaPlayerActivity;
 import com.example.cloudplaylistmanager.R;
@@ -52,6 +55,9 @@ public class SavedSongsFragment extends Fragment {
                     intent.putExtra(MediaPlayerActivity.POSITION_TAG,position);
                     startActivity(intent);
                 }
+                else {
+                    LaunchDialogPopup();
+                }
             }
         });
         songRecyclerView.setAdapter(this.songsAdapter);
@@ -66,5 +72,33 @@ public class SavedSongsFragment extends Fragment {
             }
         });
         return view;
+    }
+
+
+    public void LaunchDialogPopup() {
+        Dialog dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.create_new_song_popup);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        TextView downloadButton = dialog.findViewById(R.id.textView_download_button);
+        TextView chooseButton = dialog.findViewById(R.id.textView_choose_file);
+        TextView fileName = dialog.findViewById(R.id.textView_file_name);
+        EditText urlField = dialog.findViewById(R.id.edit_text_url);
+
+        chooseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //fileName.setText();
+            }
+        });
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 }
